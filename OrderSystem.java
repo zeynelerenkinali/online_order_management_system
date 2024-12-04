52894ec8b112d30361e09e7a46d0b3bd8ae82ad7
zeynelerenkinali <zeynel.eren.kinali55@erzurum.edu.tr>
2024-12-04 15:18:58 +0300

    add_node code finished

--- OrderSystem.java
+++ OrderSystem.java
@@ -41,7 +41,7 @@ import java.util.*;
  *      7.4 Set next_order_node's parent = cur_order_node
  *      7.5 AddOrder(order, next_order_node)
  * 
- * 8. Else if root equal to cur_order_node 
+ * 8. Else if root.data equal to cur_order_node.data 
  *      8.1 Increase root's quantity with 1
  *      8.2 Increase order's index with 1
  *      8.3 Set root.child = next_order_node
@@ -55,7 +55,9 @@ import java.util.*;
  *      9.4 Increase cur_order_node's quantity with 1
  *      9.5 Set cur_order_node's child = next_order_node
  *      9.6 Set next_order_node's parent = cur_order_node
- *      9.7 AddOrder(order, cur_order_node)
+ *      9.7 AddOrder(order, next_order_node) // this was cur_order_node I thought this might be wrong check later.
+ * 10. Else
+ *      10.1 AddOrder(order, next_order_node)
  */
 
 
@@ -78,22 +80,63 @@ public class OrderSystem
 
     public void AddOrder(String order[], Node root)
     {
-        if(isAlph == false)
+        if(isAlph == false) // 1.
         {
-            Alphabetical(order, this.alphabetType);
-            isAlph = true;
+            Alphabetical(order, this.alphabetType); 
+            isAlph = true; 
         }
-        cur_order_node = new Node(order[index]);
-        next_order_node = null;
-        if((index + 1) >= order.length)
+        cur_order_node = new Node(order[index]); // 2.
+        next_order_node = null; // 3.
+        if((index + 1) >= order.length) //4. 
         {
             cur_order_node.increase_quaintity();
             System.out.println("Order addition successfully operated");
             index = 0;
             isAlph = false;
-            // Do not call recursive will end operation
+            // Do not call recursive will end operation // return
+            return;
+        }   
+        else // 5.
+            next_order_node = new Node(order[index+1]);
+        if(this.root == null) // 6.
+        {
+            this.root = cur_order_node;
+            this.root.set_parent_node(null);
+            this.root.set_sibling_node(null);
+            this.root.increase_quaintity();
+            index++;
+            this.root.set_child_node(next_order_node);
+            next_order_node.set_parent_node(cur_order_node);
+            AddOrder(order, next_order_node);
         }
-        
+        if(root == null && this.root.get_data().equals(cur_order_node.get_data())) // 7.
+        {
+            this.root.increase_quaintity();
+            index++;
+            this.root.get_child_node().set_sibling_node(next_order_node);
+            next_order_node.set_parent_node(cur_order_node);
+            AddOrder(order, next_order_node);
+        }
+        else if(root.get_data().equals(cur_order_node.get_data())) // 8.
+        {
+            root.increase_quaintity();
+            index++;
+            root.set_child_node(next_order_node);
+            next_order_node.set_parent_node(cur_order_node);
+            AddOrder(order, next_order_node);
+        }
+        else if(root.get_sibling_node() == null)
+        {
+            root.set_sibling_node(cur_order_node);
+            cur_order_node.set_parent_node(root.get_parent_node());
+            index++;
+            cur_order_node.increase_quaintity();
+            cur_order_node.set_child_node(next_order_node);
+            next_order_node.set_parent_node(cur_order_node);
+            AddOrder(order, next_order_node);
+        }
+        else // on process 
+            AddOrder(order, next_order_node);
     }
 
 
